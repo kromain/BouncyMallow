87f63c1194589078a0d4a8778d53b343cb7a5dba
Romain Pokrzywka <romain.pokrzywka@am.sony.com>
2013-10-23 12:51:27 -0700

    Improve query arg type detection and add support for nested args

--- scripts/query-args.js
+++ scripts/query-args.js
@@ -5,15 +5,51 @@ function extractQueryArgs() {
     var queryStrings = location.search.substring(1).split('&');
     for (var i = 0; i < queryStrings.length; i++) {
       var arg = queryStrings[i].split('=');
-      queryArgs[arg[0]] = arg[1] ? Number(arg[1]) : 1;
+      if (arg.length == 1) {
+        queryArgs[arg[0]] = true;
+        continue;
+      }
+
+      var val = {};
+      queryArgs[arg[0]] = (parseNumber(arg[1], val) || parseBool(arg[1], val) || parseObject(arg[1], val)) ? val.res : arg[1];
     }
   }
+}
+
+function parseNumber(str, obj) {
+  obj.res = Number(str);
+  return !isNaN(obj.res);
+}
+
+function parseBool(str, obj) {
+  if (str !== "true" && str !== "false")
+    return false;
+
+  obj.res = (str === "true");
+  return true;
+}
+
+function parseObject(str, obj) {
+  var argv = str.split(';');
+  if (argv.length < 2)
+    return false;
+
+    obj.res = new Object();
+    for (var i = 0; i < argv.length; i+=2) {
+      if (argv[i+1].length == 0) {
+        obj.res[argv[i]] = true;
+        continue;
+      }
+
+      var val = {};
+      obj.res[argv[i]] = (parseNumber(argv[i+1], val) || parseBool(argv[i+1], val)) ? val.res : argv[i+1];
+    }
 }
 
 function buildQueryString(queryargs) {
   var queryString = "";
   for(var arg in queryargs) {
-    queryString += queryString ? "&" : "?";
+    queryString += queryString.length ? "&" : "?";
 
     if (typeof(queryargs[arg]) === "boolean" && queryargs[arg])
       queryString += arg;
@@ -22,4 +58,15 @@ function buildQueryString(queryargs) {
   }
 
   return queryString;
+}
+
+function buildArgvString(queryargs) {
+  var argvString = "";
+  for(var arg in queryargs) {
+      if (argvString.length)
+        argvString += ";";
+      argvString += arg + ";" + queryargs[arg];
+  }
+
+  return argvString;
 }
