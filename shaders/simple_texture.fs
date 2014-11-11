0ccc63f2f941d33e9c8ebc05a4fe7a0826b48f0c
Romain Pokrzywka <romain.pokrzywka@am.sony.com>
2014-11-10 18:43:27 -0800

    Add alpha premultiply support to the fragment shader (currently commented out)

--- shaders/simple_texture.fs
+++ shaders/simple_texture.fs
@@ -1,7 +1,15 @@
 precision mediump float;
 varying vec2 v_texCoord;
 uniform sampler2D s_texture;
+
+vec4 premultiply(vec4 color)
+{
+  float a = color.a;
+  return vec4(color.rgb * a, a);
+}
+
 void main()
 {
-  gl_FragColor = texture2D( s_texture, v_texCoord );
+  gl_FragColor = texture2D(s_texture, v_texCoord);
+//	gl_FragColor = premultiply(texture2D(s_texture, v_texCoord));
 }
